e605cd3b2d856d2e73826d8c902c3335a10fe16b
govind <govind.edunext@gmail.com>
2023-09-19 21:18:25 +0530

    govind

---
+++ E-Commerce/src/main/java/com/example/demoecommerceservice/ECommerceService.java
@@ -0,0 +1,5 @@
+package com.example.demoecommerceservice;
+
+public interface ECommerceService {
+
+}
---
+++ E-Commerce/src/main/java/com/example/demoecommerceservice/ECommerceServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.demoecommerceservice;
+
+public class ECommerceServiceImpl {
+
+}
